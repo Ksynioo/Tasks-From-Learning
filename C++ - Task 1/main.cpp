1887be7cae6793ed00577b6f422b6c3c3d01e45b
Ksynioo <krzstr98@gmail.com>
2024-08-04 15:10:37 +0200

    First task in C++

---
+++ C++ - Task 1/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string first_name = "Chris", second_name = "Smith";
+    int age = 26;
+
+    cout << "Your first name is: " << first_name << endl;
+    cout << "Your second name is:  " << second_name << endl;
+    cout << "You are " << age << " years old." << endl;
+
+    return 0;
+}
