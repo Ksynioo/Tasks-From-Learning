3e512bceb64dc833cd1dd0a52ba5bf14eb937c6c
Ksynioo <krzstr98@gmail.com>
2024-08-09 15:58:08 +0200

    Replaced Polish text with English

--- C++ - Task 5/main.cpp
+++ C++ - Task 5/main.cpp
@@ -5,19 +5,19 @@ using namespace std;
 
 int main()
 {
-    int liczba = 100, pobierz;
+    int number = 100, take_num;
 
     do
     {
         cin.clear();
         cin.sync();
 
-        cout << "Wprowadz liczbe calkowita : ";
-        cin >> pobierz;
+        cout << "Enter Integer Value: ";
+        cin >> take_num;
 
     }while(cin.good() < 1);
 
-    cout << "Wynik : " << liczba + pobierz << endl;
+    cout << "Result : " << number + take_num << endl;
 
     return 0;
 }
